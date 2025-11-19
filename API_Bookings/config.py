61794a4b14ceb4b5fa69037cfa41c9b4309efee3
JoseMontenegro2005 <josemontenegro@unicomfacauca.edu.co>
2025-11-18 22:05:36 -0500

    Solucion base de datos

--- API_Bookings/config.py
+++ API_Bookings/config.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
-    MYSQL_USER = os.getenv("MYSQL_USER", "user_reservas")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "reservaspass456")
-    MYSQL_DB = os.getenv("MYSQL_DB", "reservas_db") 
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 24021))
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB") 
     MYSQL_CURSORCLASS = "DictCursor"
+    
     MYSQL_SSL_DISABLED = False
     MYSQL_SSL = {'ssl': {'ca': '/etc/ssl/cert.pem'}} 
     CATALOGO_API_URL = os.getenv("CATALOGO_API_URL", "http://127.0.0.1:5001")
--- API_Tours/config.py
+++ API_Tours/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306)) 
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 24021)) 
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("MYSQL_DB")
