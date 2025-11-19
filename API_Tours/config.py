84980b05d6a62b7ec412f2353d3b473c65aafb52
JoseMontenegro2005 <josemontenegro@unicomfacauca.edu.co>
2025-11-18 21:44:57 -0500

    Solucion base de datos

--- API_Tours/config.py
+++ API_Tours/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-    MYSQL_USER = os.getenv("MYSQL_USER", "user_catalogo")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "tourpass123")
-    MYSQL_DB = os.getenv("MYSQL_DB", "catalogo_db")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306)) 
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
     MYSQL_CURSORCLASS = "DictCursor"
 
     API_KEY = os.getenv("API_KEY", "tourfer-catalogo-secret-key")
