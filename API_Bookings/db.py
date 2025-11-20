5dde9ddbabf12e7429c1d26069d95bc1441fc856
José Luis Montenegro <145773730+JoseMontenegro2005@users.noreply.github.com>
2025-11-20 10:01:05 -0500

    Enhance MySQL configuration with port and SSL CA
    Add MySQL port and SSL CA configuration with warning.

--- API_Bookings/db.py
+++ API_Bookings/db.py
@@ -8,7 +8,13 @@ def init_mysql(app, cfg):
     app.config['MYSQL_USER'] = cfg.MYSQL_USER
     app.config['MYSQL_PASSWORD'] = cfg.MYSQL_PASSWORD
     app.config['MYSQL_DB'] = cfg.MYSQL_DB
+    app.config['MYSQL_PORT'] = cfg.MYSQL_PORT
     app.config['MYSQL_CURSORCLASS'] = cfg.MYSQL_CURSORCLASS
-    
+
+    if cfg.MYSQL_SSL_CA and os.path.exists(cfg.MYSQL_SSL_CA):
+        app.config['MYSQL_SSL_CA] = cfg.MYSQL_SSL_CA
+        else:
+        print("ADVERTENCIA: No se encontró ca.pem, la conexión a Aiven podría fallar.")
+        
     mysql.init_app(app)
     return mysql
