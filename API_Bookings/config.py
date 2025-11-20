c92563ba4ecc2eeac8d37fb0e2d8b9b744e10fce
José Luis Montenegro <145773730+JoseMontenegro2005@users.noreply.github.com>
2025-11-20 10:01:50 -0500

    Update config.py

--- API_Bookings/config.py
+++ API_Bookings/config.py
@@ -7,8 +7,10 @@ class Config:
     MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
     MYSQL_USER = os.getenv("MYSQL_USER", "user_reservas")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "reservaspass456")
-    MYSQL_DB = os.getenv("MYSQL_DB", "reservas_db") 
+    MYSQL_DB = os.getenv("MYSQL_DB", "reservas_db")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
     MYSQL_CURSORCLASS = "DictCursor"
+    MYSQL_SSL_CA = os.getenv("MYSQL_SSL_CA", "ca.pem")
 
     CATALOGO_API_URL = os.getenv("CATALOGO_API_URL", "http://127.0.0.1:5001")
 
